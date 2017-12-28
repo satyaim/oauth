4343f92f4baaa53caad600e032177326cd881b46
satyaim <thisissatyavrat@gmail.com>
2017-12-28 17:16:08 +0530

    creating/checking new/old user in db

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,7 +2,7 @@
 const passport= require("passport");
 const GoogleStrategy= require("passport-google-oauth20");
 const key= require("./keys");
-
+const User= require("../models/user-model");
 // Tell Passport We're using Google Strategy
 passport.use(new GoogleStrategy(
 	{
@@ -13,6 +13,21 @@ passport.use(new GoogleStrategy(
 	function(accessToken,refreshToken,profile,done){
 		console.log("passport callback function fired");
 		console.log(profile);
+		//Match one user from User
+		User.findOne({ googleId: profile.id}).then(function(curr_user){
+			if(curr_user){
+				console.log("current user is" + curr_user);
+			}
+			else{
+					console.log("adding new user");
+				new User({
+					username: profile.displayname,
+					googleId: profile.id
+				}).save().then(function(new_user){
+					console.log("new user is" + new_user);
+				});
+			}
+		});
 	}
 ));
 
